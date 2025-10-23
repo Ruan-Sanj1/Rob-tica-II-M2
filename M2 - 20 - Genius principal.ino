58d5fa721db610b4a98c01a80979afdf3d1bc9cb
Ruan-Sanj1 <104634760+Ruan-Sanj1@users.noreply.github.com>
2025-10-23 10:52:58 -0300

    Update M2 - 20 - Genius principal.ino

--- M2 - 20 - Genius principal.ino
+++ M2 - 20 - Genius principal.ino
@@ -16,7 +16,7 @@ Serial.println(recorde);
 esperarNovoJogo();
 
 
---------------------------------------------------------------VARIÁVEIS--------------------------------------------------------------------------
+--------------------------------------------------------------VARIÁVEIS.H--------------------------------------------------------------------------
   /* Biblioteca que permitirá a gravação de dados na memória eeprom
 do Arduino. */
 #include <EEPROM.h>
@@ -51,3 +51,29 @@ int sequencia[100];
 /* Indica quantos elementos da sequência foram gerados e devem ser
 reproduzidos pelo jogador. */
 int tamanhoSequencia = 0;
+
+
+
+
+--------------------------------------------------------------CONFIGURAR.INO--------------------------------------------------------------------------
+void configurar() {
+/* Configuração dos pinos dos LEDs como saída. */
+pinMode(ledAmarelo, OUTPUT);
+pinMode(ledAzul, OUTPUT);
+pinMode(ledVerde, OUTPUT);
+pinMode(ledVermelho, OUTPUT);
+/* Configuração dos pinos dos botões como entrada pull-up. */
+pinMode(botaoAmarelo, INPUT_PULLUP);
+pinMode(botaoAzul, INPUT_PULLUP);
+pinMode(botaoVerde, INPUT_PULLUP);
+pinMode(botaoVermelho, INPUT_PULLUP);
+/* Inicialização da comunicação serial para debug. */
+Serial.begin(9600);
+/* Inicializa o gerador de números aleatórios. A função lê, pelo analogRead(0)
+como parâmetro, o valor do pino analógico do Arduino. Esse pino não está conectado em
+nada específico, então ele faz a coleta de valores aleatórios entre 0 e 1023 porque a porta
+fica em um estado flutuante, o que gera números aleatórios com um valor diferente toda
+vez que o programa é iniciado, garantindo assim que as sequências geradas para nosso
+Genius sejam diferentes a cada jogada. */
+randomSeed(analogRead(0));
+}
